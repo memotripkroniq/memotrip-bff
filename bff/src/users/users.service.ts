fbb05698c64cd06e95926b09814eb5bc6833bf7b
TO2\pe603380 <petr.kucera@o2.sk>
2025-11-29 16:58:59 +0100

    fix: changed import name

--- bff/src/users/users.service.ts
+++ bff/src/users/users.service.ts
@@ -1,7 +1,7 @@
 // src/users/users.service.ts
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
